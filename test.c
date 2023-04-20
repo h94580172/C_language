40620727fc60b0a38da6756033b0328163d5407c
Eden <h94580172@gmail.com>
2023-04-20 09:32:49 +0800

    modify

--- test.c
+++ test.c
@@ -1,9 +1,60 @@
-#include "var.c" 
+#include <stdio.h>
 
-extern int a;
+typedef unsigned char u8;
+
+enum {
+    ProjCtrlLS_USB,
+    ProjCtrlLS_AltPortGetDev,
+    ProjCtrlLS_AltPd_DCJack,
+    ProjCtrlLS_AltPd_DCEmpty,
+    ProjCtrlLS_AltDischage,
+    ProjCtrlLS_Init
+};
+
+typedef struct customization_projector_s {
+    void (*fpProjectorCtrlLS)(u8 Status);
+} custom;
+
+custom c;
+
+void ProjectorCtrlLS(u8 Status) {
+    switch (Status) {
+        case ProjCtrlLS_USB:
+            printf("USB");
+            break;
+
+        case ProjCtrlLS_AltPortGetDev:
+            printf("AltPortGetDev");
+            break;
+
+        case ProjCtrlLS_AltPd_DCJack:
+            printf("AltPd_DCJack");
+            break;
+
+        case ProjCtrlLS_AltPd_DCEmpty:
+            printf("AltPd_DCEmpty");
+            break;
+            
+        case ProjCtrlLS_AltDischage:
+            printf("AltDischage");
+            break;
+            
+        case ProjCtrlLS_Init:
+            printf("Init");
+            break;
+            
+        default:
+            printf("default");
+            break;
+    }
+}
+
+void initializeCustomization() {
+    c.fpProjectorCtrlLS = ProjectorCtrlLS;
+}
 
 int main() {
-    // 使用變數 a
-    printf("a = %d\n", a);
+    initializeCustomization();
+    c.fpProjectorCtrlLS(ProjCtrlLS_USB);
     return 0;
 }
--- var.c
+++ var.c
@@ -1,1 +1,31 @@
-int a = 10;
+#include <stdio.h>
+
+typedef union {
+    struct {
+        unsigned int ALT : 1;
+        unsigned int USB : 1;
+        unsigned int PD : 1;
+        unsigned int DC : 1;
+        unsigned int reserved : 4;
+    } st;
+    unsigned char state;
+} state_t;
+
+int main() {
+
+    state_t gv31;
+    gv31.state = 0x07;
+    if(gv31.state && gv31.st.ALT == 1) {
+        printf("ALT: %d\n", gv31.st.ALT);
+    }
+    if(gv31.state && gv31.st.USB == 1) {
+        printf("USB: %d\n", gv31.st.USB);
+    }
+    if(gv31.state && gv31.st.PD == 1) {
+        printf("PD: %d\n", gv31.st.PD);
+    }
+    if(gv31.state && gv31.st.DC == 0) {
+        printf("DC: %d\n", gv31.st.DC);
+    }
+    return 0;
+}
