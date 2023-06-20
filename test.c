70dfdd5b00b60f10be22cd24df58e61a9ab93372
Eden <h94580172@gmail.com>
2023-06-20 16:03:26 +0800

    update

--- test.c
+++ test.c
@@ -1,60 +1,16 @@
 #include <stdio.h>
 
-typedef unsigned char u8;
-
-enum {
-    ProjCtrlLS_USB,
-    ProjCtrlLS_AltPortGetDev,
-    ProjCtrlLS_AltPd_DCJack,
-    ProjCtrlLS_AltPd_DCEmpty,
-    ProjCtrlLS_AltDischage,
-    ProjCtrlLS_Init
-};
-
-typedef struct customization_projector_s {
-    void (*fpProjectorCtrlLS)(u8 Status);
-} custom;
-
-custom c;
-
-void ProjectorCtrlLS(u8 Status) {
-    switch (Status) {
-        case ProjCtrlLS_USB:
-            printf("USB");
-            break;
-
-        case ProjCtrlLS_AltPortGetDev:
-            printf("AltPortGetDev");
-            break;
-
-        case ProjCtrlLS_AltPd_DCJack:
-            printf("AltPd_DCJack");
-            break;
-
-        case ProjCtrlLS_AltPd_DCEmpty:
-            printf("AltPd_DCEmpty");
-            break;
-            
-        case ProjCtrlLS_AltDischage:
-            printf("AltDischage");
-            break;
-            
-        case ProjCtrlLS_Init:
-            printf("Init");
-            break;
-            
-        default:
-            printf("default");
-            break;
-    }
-}
-
-void initializeCustomization() {
-    c.fpProjectorCtrlLS = ProjectorCtrlLS;
-}
-
 int main() {
-    initializeCustomization();
-    c.fpProjectorCtrlLS(ProjCtrlLS_USB);
-    return 0;
+    long ans = 0;
+    short a = 0x1234;
+    short b = 0x5600;
+    ans += a << 16;
+    ans += b << 0;
+    ans += (b >> 2) + 0x22;
+    printf("a=%x\n",ans);
 }
+// 1234 5600 +1580+0022
+// 1234 1580
+//      0022
+// 1234 6BA2
+// 1 5 8 0
