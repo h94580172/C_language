341715b8aaefac481d09bd06d72b6210e9ab518f
eden.lai <h94580172@gmail.com>
2023-06-27 02:20:09 +0800

    update

--- test.c
+++ test.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#define MIN(a,b) (a < b ? a : b)
+#ifndef DEBUG
+#error "12354564564"
+#endif
 
 int bubbleSort(int arr[],int n){
     for(int i=0; i<n-1 ;i++){
@@ -13,15 +17,22 @@ int bubbleSort(int arr[],int n){
         printf("\n");
     }
 }
+ 
+// int main()
+// {
+//     int arr[] = {64, 34, 25, 12, 22, 11, 90};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     bubbleSort(arr, n);
+//     printf("Sorted array: \n");
+//     for (int i=0; i < n; i++)
+//         printf("%d ", arr[i]);
+//     return 0;
+// }
+
 
 int main()
 {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
-    printf("Sorted array: \n");
-    for (int i=0; i < n; i++)
-        printf("%d ", arr[i]);
+    printf("min:%d\n",MIN(88,6));
     return 0;
 }
 
