b9e9341d954882d0b82c5a449042c0dd658890ec
Eden <h94580172@gmail.com>
2023-06-26 18:19:08 +0800

    update

--- test.c
+++ test.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
+int bubbleSort(int arr[],int n){
+    for(int i=0; i<n-1 ;i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i] > arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+            printf("%d ",arr[j]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
-    int arr[3][4];
-    printf("1:%d\n", sizeof(arr));  // 結果為 48（3 * 4 * 4(int)）
-
-    char str[2][10];
-    printf("2:%d\n", sizeof(str));  // 結果為 20（2 * 10 * 1(char)）
-
-    int* ptr[5][3];
-    printf("3:%d\n", sizeof(ptr));  // 結果為 120（5 * 3 * 8(int*)）
-
-    struct Point 
-    {
-        int x;
-        int y;
-    };
-
-    struct Point points[4][3];
-    printf("4:%d\n", sizeof(points));  // 結果為 96（4 * 3 * 8(struct Point)）
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    for (int i=0; i < n; i++)
+        printf("%d ", arr[i]);
     return 0;
 }
+
