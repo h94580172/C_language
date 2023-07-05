179c9f4e35ce115d7695357f6e7d357107cd8410
Eden <h94580172@gmail.com>
2023-07-05 19:03:17 +0800

    update

--- test.c
+++ test.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-void function(int num){
-    int space,star;
-    int mid = num/2;
-    for(int i=0; i<=mid; i++){
-        for(space=i; space<mid; space++){
-            printf(" ");
-        }
-        for(star=0; star<=i; star++){
-            printf("*");
-        }
-        printf("\n");
-    }
-    for(int i=mid-1; i>0; i--){
-        for(space=i; space<mid; space++){
-            printf(" ");
-        }
-        printf("\n");
-    }
+
+void func1(){
+    printf("a");
+}
+
+void func2(){
+    printf("b");
 }
 
 int main() {
-    function(5);
-    return 0;
+    int a[5] = {1, 2, 3, 4, 5};
+    int *p = (int*)(&a+1);
+    // printf("%d\n",p);
+    // printf("%d\n",*(a+1));
+    printf("%d\n",(*p-1));
 }
