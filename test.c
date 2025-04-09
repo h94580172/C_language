b5f4bfde4ce6930bd4cfe0a6efeacb1ef8b2e7e0
Eden <h94580172@gmail.com>
2025-04-09 17:37:09 +0800

    update

--- test.c
+++ test.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
-int n2(int n){
-    return ((n > 0) && (n&(n-1)) == 0);
-}
-
-int main(void)
+int main()
 {
-    printf("\n%d",n2(8));
+    int i = 5;
+    int j = i++;
+    printf("i1 = %d\n",i);  
+    printf("j1 = %d\n",j);  
+
+    i = 5;
+    j = ++i; 
+    printf("i2 = %d\n",i);
+    printf("j2 = %d\n",j);  
+
+    int A=2 , AA=0;
+    int B=2 , BB=0;
+
+    AA = (A++) + (A++);
+    BB = (++B) + (B++);
+    printf("A=%d   B=%d\n",AA,BB);
 
     return 0;
 }
