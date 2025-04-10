72f0b362ae5fcc105688dfaf2b07d49e41f37a98
Eden <h94580172@gmail.com>
2025-04-10 17:50:38 +0800

    update

--- test.c
+++ test.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
+int func(int num){
+    int sum = 0;
+    while(num){
+        num &= (num-1);
+        sum++;
+    }
+    return sum;
+
+}
 int main()
 {
-    int i = 5;
-    int j = i++;
-    printf("i1 = %d\n",i);  
-    printf("j1 = %d\n",j);  
-
-    i = 5;
-    j = ++i; 
-    printf("i2 = %d\n",i);
-    printf("j2 = %d\n",j);  
-
-    int A=2 , AA=0;
-    int B=2 , BB=0;
-
-    AA = (A++) + (A++);
-    BB = (++B) + (B++);
-    printf("A=%d   B=%d\n",AA,BB);
+    printf("sum=%d\n",func(9));
 
     return 0;
 }
