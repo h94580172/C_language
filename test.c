74b110f674723b65ccc59be7610117bc7f416307
Eden <h94580172@gmail.com>
2025-06-03 00:41:40 +0800

    update

--- test.c
+++ test.c
@@ -1,16 +1,58 @@
 #include <stdio.h>
 
-int main(){
-    int arr1[] = {10,20};
-    int arr2[] = {10,20};
-    int arr3[] = {10,20};
-    int *p = arr1;
-    int *q = arr2;
-    int *r = arr3;
-    ++*p;
-    *q++;
-    *++r;
-    printf("%d %d %d\n", arr1[0], arr1[1], *p);11 20 11
-    printf("%d %d %d\n", arr2[0], arr2[1], *q);10 20 20
-    printf("%d %d %d\n", arr3[0], arr3[1], *r);11 20 11
+typedef struct MyStruct1
+{
+    char a[2];
+    int b;
+    double c;
+    int *Pint;
+    char d;
+    char* Pchar;
+} MyStruct1;
+
+typedef struct MyStruct2
+{
+    char a;
+    char b;
+    struct str2
+    {
+        int d;
+    } c;
+}MyStruct2;
+
+typedef struct MyStruct3
+{
+    char a;
+    int b;
+    char c;
+    char* d;
+    double* e;
+    struct str3
+    {
+        int f;
+        char g;
+        struct str33
+        {
+            char* p;
+        }n;
+    } m;
+}MyStruct3;
+
+typedef struct MyStruct4
+{
+    char a;
+    char b;
+    struct str4
+    {
+        char c;
+        char d;
+    };
+}MyStruct4;
+
+int main() {
+    printf("1. %zu\n", sizeof(MyStruct1));4+4+8+8+8+8=40
+    printf("2. %zu\n", sizeof(MyStruct2));1+1+2+4=8
+    printf("3. %zu\n", sizeof(MyStruct3));4+4+8+8+8+4+8+8=52
+    printf("4. %zu\n", sizeof(MyStruct4));1+1
+    return 0;
 }
